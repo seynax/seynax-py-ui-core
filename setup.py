aa40d646e6dee83139e0d52c4a723286ed824eda
Seynax <seyronax@gmail.com>
2024-02-16 09:34:17 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='seynax_py_ui_core',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/seynax/seynax-py-ui-core',
+    license='GNU General Public License v3.0',
+    author='Seynax',
+    author_email='seynax@onsiea.com',
+    description='Code used by Seynax for UI into multiples python projects'
+)
